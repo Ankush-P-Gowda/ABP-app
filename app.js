5efd0bb15ea7682d9e4fdf2c2d51838be5bb753f
ankush p gowda <psagowdruias2028@gmail.com>
2020-07-30 23:02:14 +0530

    my first update

---
+++ app.js
@@ -0,0 +1,16 @@
+let express = require('express'),
+bodyParser = require('body-parser'),
+app = express(),
+port = process.env.PORT || 3000;
+
+app.set('view engine','ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/',function(req,res){
+    res.render("landing");
+});
+
+app.listen(port,function(){
+    console.log("Server Started Ankush..!!");
+});
