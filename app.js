7df681610795f6343b8aa80a99a699a3f3590484
ankush p gowda <psagowdruias2028@gmail.com>
2020-08-01 00:15:25 +0530

    my first update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',function(req,res){
-    res.render("landing");
+    res.render("main/landing");
 });
 
 app.listen(port,function(){
