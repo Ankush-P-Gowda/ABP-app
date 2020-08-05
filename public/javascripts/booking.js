a1ed9dca7330caf29565b0deff706debe298dc96
ankush p gowda <psagowdruias2028@gmail.com>
2020-08-05 18:24:12 +0530

    my third update

--- public/javascripts/booking.js
+++ public/javascripts/booking.js
@@ -1,3 +1,20 @@
+var dt = new Date();
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+ 
+function loadDate(){
+  document.getElementsByClassName("month")[0].innerText = months[dt.getMonth()];
+  document.getElementsByClassName("year")[0].innerText = dt.getFullYear();
+}
+function movePara(para){
+  if(para == 'prev'){
+    dt.setMonth(dt.getMonth()-1);
+    loadDate();
+  }else{
+    dt.setMonth(dt.getMonth()+1);
+    loadDate();
+  }
+}
+
 $(document).ready(function(){
     $('.date').slick({
         centerMode: true,
@@ -17,3 +34,10 @@ $(document).ready(function(){
     autoplaySpeed: 2000,
 });
 });
+
+
+
+
+
+
+
