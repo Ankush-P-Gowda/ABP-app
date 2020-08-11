5d03c5fc8709a6ea2ae6ab671c658461ef32a916
ankush p gowda <psagowdruias2028@gmail.com>
2020-08-11 22:41:05 +0530

    my third update

--- public/javascripts/booking.js
+++ public/javascripts/booking.js
@@ -1,6 +1,9 @@
 var dt = new Date();
 var start = new Date();
+var today = new Date();
 var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+var j = 0;
+    // dt.setMonth(11);start.setMonth(11);today.setMonth(11);
 
 function RenderCalender(){
     var endDate = new Date(
@@ -31,35 +34,51 @@ function RenderCalender(){
   document.getElementsByClassName("months")[0].innerText = months[dt.getMonth()];
   document.getElementsByClassName("year")[0].innerText = dt.getFullYear();
   for(x=sday; x>0; x--){
-      cell+="<p class='shadecolor prevmonth'>"+(prevDate-x)+"</p>"
+    //   cell+="<p class='shadecolor prevmonth' onclick=\"movePara(\'prev\')\">"+(prevDate-x+1)+"</p>"
+      cell+="<label class=\"radiodates\"><input type=\"radio\" name=\"radio\" id=''><span class=\"radio-btn shadecolor prevmonth\" onclick=\"movePara(\'prev\')\">" +(prevDate-x+1)+ "</span></label>"
   }
   for(i=1; i<=endDate; i++){
-      cell+="<p>"+i+"</p>";
+      if(i==today.getDate() && dt.getMonth()==today.getMonth()){
+    //   cell+="<p class='pink'>"+i+"</p>";
+      cell+="<label class=\"radiodates\"><input type=\"radio\" name=\"radio\" id=''><span class=\"radio-btn pink\">" +i+ "</span></label>"
+      }else{
+        // cell+="<p>"+i+"</p>";
+        cell+="<label class=\"radiodates\"><input type=\"radio\" name=\"radio\" id=''><span class=\"radio-btn\">" +i+ "</span></label>"
+
+      }
   }
   if(endDay!=6){
     for(y=eday,i=1 ;y<6 ; y++,i++){
-        cell+="<p class='shadecolor nextmonth'>"+i+"</p>";
+        // cell+="<p class='shadecolor nextmonth' onclick=\"movePara(\'next\')\">"+i+"</p>";
+        cell+="<label class=\"radiodates\"><input type=\"radio\" name=\"radio\" id=''><span class=\"radio-btn shadecolor nextmonth\" onclick=\"movePara(\'next\')\">" +i+ "</span></label>"
   }
 }
   document.getElementsByClassName("date")[0].innerHTML = cell;
 }
 
-
 function movePara(para){
     let i = dt.getMonth();
-    if(para == 'prev'){
+    if(para == 'prev' && j > 0){
         i--;
         dt.setMonth(i);
         console.log(i);
+        console.log(j);
         start.setMonth(i);
-    }else if(para == 'next'){
+        j=j-1;
+    }else if(para == 'next' && j < 3){
         i++;
         dt.setMonth(i);
         console.log(i);
+        console.log(j);
         start.setMonth(i);
+        j=j+1;
     }
     RenderCalender();
 }
+
+
+
+
 
 
 
