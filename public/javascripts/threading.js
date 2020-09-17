feaf117ac5a5318facd03b7b665765b1e03fa5b5
ankush p gowda <psagowdruias2028@gmail.com>
2020-09-17 21:54:43 +0530

    i am updating

--- public/javascripts/threading.js
+++ public/javascripts/threading.js
@@ -1,5 +1,16 @@
 var price = document.querySelectorAll('.col-8 h5:nth-of-type(2)');
+var addbtn = document.querySelectorAll('.col-4 .btn');
+
 
 function pricecheck(i,j){
     price[i].innerText = 'Rs.' + j
 }
+
+
+$(function(){
+    $('.col-4 .btn').on('click',function(){
+        $('.bookingnav').addClass('animate__animated animate__heartBeat').one('webkitAnimationEnd animationed',function(){
+            $(this).removeClass('animate__animated animate__heartBeat')
+        });
+    }); 
+});
